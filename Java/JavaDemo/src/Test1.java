945f9a6fe27cd29f7eefe27588fd3db7685d2658
Dipen <ddelvadi@hawk.iit.edu>
2019-04-18 19:39:00 -0400

    Deleting unnecessary file

--- Java/JavaDemo/src/Test1.java
+++
@@ -1,9 +0,0 @@
-
-public class Test1 {
-	public static void main(String args[]) {
-		System.out.println("Connecting Eclipse project with github!!");
-		System.out.println("Still Testing");
-		
-		
-	}
-}
